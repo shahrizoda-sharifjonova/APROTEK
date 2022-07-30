113df304cbbe7b1a3e9746fafb680080459fc03b
shahrizoda <shahrizodadeveloping@gmail.com>
2022-07-30 12:09:27 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,27 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-// const swiper = new Swiper();
+const swiper = new Swiper();
 
+new Swiper(".about__swiper", {
+    spaceBetween: 10,
+    slidesPerView: 'auto',
+    breakpoints:{
+        1200:{
+            slidesPerView: 4,
+        },
+        992:{
+            slidesPerView: 3,
+        },
+        576:{
+            slidesPerView: 2,
+        }
+    },
+  });
+
+  
 const navbarToggler = document.querySelector('.navbar-toggler');
 const navbarCollapse = document.querySelector('.navbar-collapse');
 const body = document.querySelector('body');
@@ -15,3 +32,4 @@ navbarToggler.addEventListener('click', (e)=>{
     navbarCollapse.classList.toggle('active')
     body.classList.toggle('hidden')
 })
+
